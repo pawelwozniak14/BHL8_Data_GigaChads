dbeb1e42799f85583a6b572d69aa50c2a1e823e4
Chajf <patrykmarek8@gmail.com>
2023-04-15 17:38:40 +0200

    corrploty

--- visualization.R
+++ visualization.R
@@ -1,1 +1,16 @@
 ## Wizualizacja
+
+library(tidyverse)
+library(ggmosaic)
+library(ggcorrplot)
+
+ggplot(na.omit(model_data))+
+  geom_mosaic(aes(x = product(Machine_failure), fill=Type), show.legend = F)+
+  labs(title = "Zależność usterk od jakościo produktu")+
+  theme_mosaic()+
+  theme(plot.title = element_text(hjust = 0.5,size=22))
+##jedyny mozaikowy który ma chyba sens, ale może lepiej boxplotem
+
+model.matrix(~0+., data=model_data[,-c(1:2)]) %>% 
+  cor(use="pairwise.complete.obs") %>% 
+  ggcorrplot(show.diag=FALSE, type="lower", lab=TRUE, lab_size=2)
