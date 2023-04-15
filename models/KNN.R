22cc2408afe5ebbd64bdf01362a3f4ac1fe7569f
Michciak <Michciak17@gmail.com>
2023-04-15 23:58:12 +0200

    knn2

--- models/KNN.R
+++ models/KNN.R
@@ -1,4 +1,5 @@
 library(tidymodels)
+library(themis)
 load("data/model_data.rda")
 
 rec <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotational_speed+Torque+Tool_wear, data=train_data) %>% 
@@ -6,19 +7,18 @@ rec <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotationa
   step_smote(Machine_failure) %>% 
   prep()
 
-rf <- rand_forest(mtry = tune(),
-                  trees = tune(),
-                  min_n = tune()) %>% 
+knn <- nearest_neighbor(weight_func = tune(),
+                       dist_power = tune()) %>% 
   set_mode("classification") %>% 
-  set_engine("ranger")
+  set_engine("kknn")
 
-rf_wf <- workflow() %>%
-  add_model(rf) %>% 
+knn_wf <- workflow() %>%
+  add_model(knn) %>% 
   add_recipe(rec)
 
 res <- vfold_cv(train_data, v = 10, strata="Machine_failure")
 
-params <- extract_parameter_set_dials(rf)
+params <- extract_parameter_set_dials(knn)
 params <- finalize(params, train_data)
 
 grid <- grid_latin_hypercube(params, size = 15)
@@ -26,40 +26,40 @@ grid <- grid_latin_hypercube(params, size = 15)
 met <- metric_set(f_meas, roc_auc, accuracy, sensitivity, specificity, recall, j_index)
 
 library(doParallel)
-registerDoParallel(cores = 5)
-rf_res <-
-  rf_wf %>%
+registerDoParallel(cores = 8)
+knn_res <-
+  knn_wf %>%
   tune_grid(
     resamples = res,
     grid = grid,
     metrics = met)
 
-rf_res %>% 
+knn_res %>% 
   collect_metrics() %>% 
   flextable::flextable()
 
-rf_res %>% 
+knn_res %>% 
   show_best("specificity")
 
-rf_best_param <- select_best(rf_res, "specificity")
+knn_best_param <- select_best(knn_res, "specificity")
 
-rf_final <- rf_wf %>% 
-  finalize_workflow(rf_best_param)
+knn_final <- knn_wf %>% 
+  finalize_workflow(knn_best_param)
 
 rec_test_data <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotational_speed+Torque+Tool_wear, data=test_data) %>% 
   step_dummy(all_factor_predictors()) %>% 
   prep() %>% 
   juice()
 
-rf_fit <- rf_final %>% 
-  last_fit(rf_final, metrics=met, split=split)
+knn_fit <- knn_final %>% 
+  last_fit(knn_final, metrics=met, split=split)
 
-rf_fit <- rf_final %>% 
+knn_fit <- knn_final %>% 
   fit(data=train_data)
 
-pred <- predict(rf_fit, new_data=test_data)
+pred <- predict(knn_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
 conf_mat(pred, truth=Machine_failure, estimate=.pred_class)
 
 
-saveRDS(rf_fit, "models/rf.rds")
+saveRDS(knn_fit, "models/knn.rds")
