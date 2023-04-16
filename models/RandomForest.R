1412d6cce95af3f76bf221f9965a4738eeed7361
Michciak <Michciak17@gmail.com>
2023-04-16 08:54:48 +0200

    Revert "rapor"
    This reverts commit 13178e82e455411096579f3b2e378c99c620c664.

--- models/RandomForest.R
+++ models/RandomForest.R
@@ -7,19 +7,19 @@ rec <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotationa
   step_smote(Machine_failure) %>% 
   prep()
 
-bg <- rand_forest(mtry = 6,
+rf <- rand_forest(mtry = tune(),
                   trees = tune(),
                   min_n = tune()) %>% 
   set_mode("classification") %>% 
   set_engine("ranger")
 
-bg_wf <- workflow() %>%
-  add_model(bg) %>% 
+rf_wf <- workflow() %>%
+  add_model(rf) %>% 
   add_recipe(rec)
 
 res <- vfold_cv(train_data, v = 10, strata="Machine_failure")
 
-params <- extract_parameter_set_dials(bg)
+params <- extract_parameter_set_dials(rf)
 params <- finalize(params, train_data)
 
 grid <- grid_latin_hypercube(params, size = 15)
@@ -27,38 +27,38 @@ grid <- grid_latin_hypercube(params, size = 15)
 met <- metric_set(f_meas, roc_auc, accuracy, sensitivity, specificity, recall, j_index)
 
 library(doParallel)
-registerDoParallel(cores = 8)
-bg_res <-
-  bg_wf %>%
+registerDoParallel(cores = 5)
+rf_res <-
+  rf_wf %>%
   tune_grid(
     resamples = res,
     grid = grid,
     metrics = met)
 
-bg_res %>% 
+rf_res %>% 
   collect_metrics() %>% 
   flextable::flextable()
 
-bg_res %>% 
+rf_res %>% 
   show_best("specificity")
 
-bg_best_param <- select_best(bg_res, "specificity")
+rf_best_param <- select_best(rf_res, "specificity")
 
-bg_final <- bg_wf %>% 
-  finalize_workflow(bg_best_param)
+rf_final <- rf_wf %>% 
+  finalize_workflow(rf_best_param)
 
 rec_test_data <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotational_speed+Torque+Tool_wear, data=test_data) %>% 
   step_dummy(all_factor_predictors()) %>% 
   prep() %>% 
   juice()
 
-bg_fit <- bg_final %>% 
+rf_fit <- rf_final %>% 
   last_fit(rf_final, metrics=met, split=split)
 
-bg_fit <- bg_final %>% 
+rf_fit <- rf_final %>% 
   fit(data=train_data)
 
-pred <- predict(bg_fit, new_data=test_data)
+pred <- predict(rf_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
 metrics(pred, truth=Machine_failure, estimate=.pred_class)
 conf_mat <- caret::confusionMatrix(pred$Machine_failure, pred$.pred_class)
@@ -66,7 +66,7 @@ conf_mat
 conf_mat(pred, truth=Machine_failure, estimate=.pred_class)
 
 
-save(bg_fit,bg_final,bg_res, file="models/bg.rda")
+save(rf_fit,rf_final,rf_res, file="models/rf.rda")
 
 
 
