70f4aeb61c5ea3d572879394f4486612082f1464
Chajf <patrykmarek8@gmail.com>
2023-04-16 02:32:38 +0200

    Działające explainery

--- model_audit.R
+++ model_audit.R
@@ -1,22 +1,30 @@
 library(tidymodels)
 library(DALEX)
+library(DALEXtra)
 
 xgb <- readRDS("models/xgb.rds")
 
-rec_test_data <- recipe(Machine_failure~Type+Air_temperature+Process_temperature+Rotational_speed+Torque+Tool_wear, data=test_data) %>% 
-  step_dummy(all_factor_predictors()) %>% 
-  prep() %>% 
-  juice()
-
-explain_xgb <- explain(model = xgb,
-                       data = test_data[,-c(1:2,9:14)],
-                       y = test_data[,9] == "0",
-                       type = "classification",
-                       label = "XGBoost",
-                       predict_function = predict)
-
-model_parts(explainer = explain_xgb,
-            B = 10) %>% 
-  plot()
+test_data <- test_data[,c(3:9)]
 
 rf <- readRDS("models/rf.rds")
+
+explainer_xgb <- explain_tidymodels(xgb,
+                                    data = test_data[,-7],
+                                    y = as.numeric(test_data$Machine_failure)-1)
+
+explainer_rf <- explain_tidymodels(rf,
+                                    data = test_data[,-7],
+                                    y = as.numeric(test_data$Machine_failure)-1,
+                                   label = "Random forest")
+
+model_profile(explainer = explainer_xgb, "Torque",
+              type = "partial")
+
+m1 <- model_parts(explainer = explainer_xgb,
+            B=5)
+
+m2 <- model_parts(explainer = explainer_rf,
+                  B=5)
+
+plot(m1,m2)
+plot(m2)
